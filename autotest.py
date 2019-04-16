1ae31541312e84943c344f7b40b634a614d38f5f
yuta <chm.af1yel.x@gmail.com>
2019-04-16 16:43:58 +0900

    Update autotest.py

--- autotest.py
+++ autotest.py
@@ -4,7 +4,6 @@ import subprocess
 import datetime
 
 cmd = 'py.test -v --capture=no test.py'
-name = 'log_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt'
 
 def test():
     log = subprocess.run(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -15,15 +14,13 @@ def testtype(subject):
     writelog(log)
 
 def writelog(log):
+    name = 'log_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt'
     content = log.stdout.decode('utf8').strip()
     logfile = open(name, 'w')
     logfile.write(content)
     logfile.close()
 
-    logcheck()
-
-def logcheck():
-    file = open(name)
+    file = open(name, 'r')
     data = file.readlines()
     file.close()
 
@@ -36,11 +33,14 @@ def logcheck():
 
 if __name__ == '__main__':
     args = sys.argv
-    while(True):
+    i = 0
+    while i < 10:
         if len(args) == 1:
             test()
+            i += 1
         elif len(args) == 2:
             testtype(args[1])
+            i += 1
         else:
             print('ERROR')
             sys.exit()
